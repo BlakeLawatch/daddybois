d26799d1dded9f383bec671a60f5dddd4be2638f
jakew33 <jacob_winfrey@yahoo.com>
2024-04-11 20:43:10 -0600

    HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Blake Likes Donkey Dong</div>
+  <div class="container-fluid">
+    <div class="col-md-4">
+      <h1>LOCALLY OWNED...
+
+        <p> Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
+          possible is very important to us as we source as many products locally as we can</p>
+      </h1>
+    </div>
+  </div>
 </template>
 
 
