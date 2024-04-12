22e80bc117f61915094a2ae7e74b79ee537715d1
jakew33 <jacob_winfrey@yahoo.com>
2024-04-11 22:47:31 -0600

    Blake sux

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
-    <div class="row">
-      <div class="col-md-6">
-        <h1>LOCALLY OWNED...</h1>
-        <p>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
-          possible is very important to us as we source as many products locally as we can.</p>
-      </div>
-      <div class="col-md-6">
-        <img alt="logo" src="src/assets/img/IMG-2964 1.png" />
+  <div class="background-image vh-100 d-flex justify-content-center align-items-center">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>LOCALLY OWNED...</h1>
+          <b>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
+            possible is very important to us as we source as many products locally as we can.</b>
+        </div>
+        <div class="col-md-6">
+          <img alt="logo" src="src/assets/img/IMG-2964 1.png" />
+        </div>
       </div>
     </div>
   </div>
@@ -31,4 +33,10 @@ export default {
 </script>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.background-image {
+  background-image: url('C:\\Users\\jacob\\source\\codeworks\\MyStuff\\goldys_corner\\src\\assets\\img\\ParchmentPaper.png');
+  background-size: cover;
+  background-position: center;
+}
+</style>
