6adba23b856d1eadf86adb9f94a858052696a449
jakew33 <jacob_winfrey@yahoo.com>
2024-04-12 17:50:07 -0600

    Stuff

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="background-image vh-100 d-flex justify-content-center align-items-center">
-    <div class="container-fluid">
+  <div class="background-image vh-100 d-flex justify-content-center align-items-center mx-auto">
+    <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <h1>LOCALLY OWNED...</h1>
-          <b>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
-            possible is very important to us as we source as many products locally as we can.</b>
+          <div class="text mx-md-auto pt-5">
+            <h1>LOCALLY OWNED...</h1>
+            <b>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
+              possible is very important to us as we source as many products locally as we can.</b>
+          </div>
         </div>
         <div class="col-md-6">
-          <img alt="logo" src="src/assets/img/IMG-2964 1.png" />
+          <img alt="logo" src="src/assets/img/IMG-2964 1.png" class="img-fluid" />
         </div>
       </div>
     </div>
@@ -38,5 +40,9 @@ export default {
   background-image: url('C:\\Users\\jacob\\source\\codeworks\\MyStuff\\goldys_corner\\src\\assets\\img\\ParchmentPaper.png');
   background-size: cover;
   background-position: center;
+}
+
+.text {
+  max-width: 400px;
 }
 </style>
