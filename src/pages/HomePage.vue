070314eda341bffb80eff99230cbfcb22b17bb3c
jakew33 <jacob_winfrey@yahoo.com>
2024-04-11 22:34:22 -0600

    home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="container-fluid">
-    <div class="col-md-4">
-      <h1>LOCALLY OWNED...
-
-        <p> Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
-          possible is very important to us as we source as many products locally as we can</p>
-      </h1>
+  <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>LOCALLY OWNED...</h1>
+        <p>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
+          possible is very important to us as we source as many products locally as we can.</p>
+      </div>
+      <div class="col-md-6">
+        <img alt="logo" src="src/assets/img/IMG-2964 1.png" />
+      </div>
     </div>
   </div>
 </template>
