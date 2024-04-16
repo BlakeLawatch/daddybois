8ae1de5c073de7e518874bb19068a5bcddbbdc8d
Blake Lawatch <blakelawatch@gmail.com>
2024-04-15 20:46:19 -0600

    text slideshow

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,11 +3,16 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
+          <div class="text mx-md-auto pt-5 slideshowContent slideshowContent" v-for="(text, index) in texts" :src="text"
+            :key="text" :style="{ display: textIndex === index ? 'block' : 'none' }">
+            <p>{{ text }} </p>
+          </div>
+          <!-- <div class="col-md-6">
           <div class="text mx-md-auto pt-5">
             <h1>LOCALLY OWNED...</h1>
             <b>Both Goldy's Corner and Goldys Breakfast Bistro are owned by the same family. Supporting local as much as
               possible is very important to us as we source as many products locally as we can.</b>
-          </div>
+          </div> -->
         </div>
         <div class="col-md-6">
           <div class="slideshowContent slideshowContent" style="max-width:500px">
@@ -29,15 +34,25 @@ export default {
         { src: "src/assets/img/coffee.png" },
         { src: "src/assets/img/food.png" },
         { src: "src/assets/img/drinks.png" }
+      ],
+      textIndex: 0,
+      texts: [
+        "butt",
+        "weiner",
+        "boobs"
       ]
     };
   },
   mounted() {
     setInterval(this.nextImage, 4000);
+    setInterval(this.nextText, 4000)
   },
   methods: {
     nextImage() {
       this.imageIndex = (this.imageIndex + 1) % this.images.length;
+    },
+    nextText() {
+      this.textIndex = (this.textIndex + 1) % this.texts.length;
     }
   }
 };
