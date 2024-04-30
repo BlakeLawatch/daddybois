bec6b6d06a1cf99aa879d70a862511d091671e09
Blake Lawatch <blakelawatch@gmail.com>
2024-04-29 18:16:50 -0600

    form better

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -6,24 +6,33 @@
       </div>
       <div class="col-12 col-md-5">
         <form>
-          <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
-          </div>
-          <div class="mb-3">
-            <label for="subject" class="form-label">Subject</label>
-            <input type="text" class="form-control" id="subject">
+          <div class="col-12 d-flex justify-content-between">
+            <div class="col-5">
+              <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" aria-describedby="emailHelp" required minlength="5"
+                  maxlength="25">
+              </div>
+            </div>
+            <div class="col-5">
+              <div class="mb-3">
+                <label for="subject" class="form-label">Subject</label>
+                <select name="subject" id="subject" class="w-100 py-1">
+                  <option :value="subjects" v-for="subject in subjects" :key="subject">{{ subject }}</option>
+                </select>
+              </div>
+            </div>
           </div>
           <div class="mb-3 form-check">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" required maxlength="50">
           </div>
           <div class="mb-3">
-            <label for="Description" class="form-label">Description</label>
-            <input type="text" class="form-control" id="Description">
+            <label for="description" class="form-label">Description</label>
+            <textarea name="description" id="description" cols="80" rows="5" required minlength="10"
+              maxlength="200"></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-danger text-end">Submit</button>
         </form>
       </div>
     </section>
@@ -37,7 +46,10 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 export default {
   setup() {
-    return {}
+    const subjects = ["Catering", "Jobs", "Art", "Other"]
+    return {
+      subjects
+    }
   }
 };
 </script>
