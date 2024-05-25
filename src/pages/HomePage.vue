f28811d961d29f022881185c05589c68cc9fff4b
Blake Lawatch <blakelawatch@gmail.com>
2024-05-25 17:49:34 -0600

    small changes

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,11 +6,11 @@
         <div class="col-md-6">
           <div class="pt-5 slideshowContent text-center slide-in" v-for="(text, index) in texts" :src="text"
             :key="index" :style="{ display: textIndex === index ? 'block' : 'none' }">
-            <p class="fs-1 fw-bold">{{ text }} </p>
+            <p class="fs-1 fw-bold arima">{{ text }} </p>
           </div>
           <div class="slideshowContent text-center" v-for="(paragraph, index) in paragraphs" :src="paragraph"
             :key="index" :style="{ display: paragraphIndex === index ? 'block' : 'none' }">
-            <p>{{ paragraph }} </p>
+            <p class="fs-4 fw-bold">{{ paragraph }} </p>
           </div>
         </div>
         <div class="col-md-6">
