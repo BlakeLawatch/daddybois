e5d16f503615b8cf4a93ba5b4eaa29c2d9d69564
Blake Lawatch <blakelawatch@gmail.com>
2024-06-04 11:11:52 -0600

    home

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,42 +1,88 @@
 <template>
   <Navbar />
-  <div class="background-image vh-100 d-flex justify-content-center align-items-center mx-auto">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="pt-5 slideshowContent text-center slide-in underline" v-for="(text, index) in texts" :src="text"
-            :key="index" :style="{ display: textIndex === index ? 'block' : 'none' }">
-            <p class="fs-1 fw-bold arima">{{ text }} </p>
-          </div>
-          <div class="slideshowContent text-center" v-for="(paragraph, index) in paragraphs" :src="paragraph"
-            :key="index" :style="{ display: paragraphIndex === index ? 'block' : 'none' }">
-            <p class="fs-4 fw-bold">{{ paragraph }} </p>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="slideshowContent">
-            <img v-for="(image, index) in images" :key="index" :src="image.src"
-              :style="{ display: imageIndex === index ? 'block' : 'none' }">
+  <div class="container-fluid background-image">
+    <section class="row slideshowContent">
+      <div class="col-12 image-container d-flex justify-content-center">
+        <img v-for="(image, index) in images" :key="index" :src="image.src"
+          :style="{ display: imageIndex === index ? 'block' : 'none' }">
+
+        <div class="col-md-5 col-10 position-absolute end-0 top-50 translate-middle-y">
+          <div class="glassCard p-4 text-light" v-for="(paragraph, index) in paragraphs" :key="index"
+            :style="{ display: paragraphIndex === index ? 'block' : 'none' }">
+            <h4>{{ paragraph }}</h4>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+    <section class="row text-center text-light p-5 justify-content-around align-items-center">
+      <div class="col-md-3 col-10 m-2">
+        <div class="card p-4 gc text-light">
+          <router-link :to="{ name: 'Bakery' }" class="btn select-borderless text-uppercase" title="Bakery Page">
+            <h4>Our Bakery</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </router-link>
+        </div>
+      </div>
+      <div class="col-md-3 col-10 m-2">
+        <div class="card p-4 gc text-light">
+          <router-link :to="{ name: 'Shop' }" class="btn select-borderless text-uppercase" title="Shop Page">
+            <h4>Merchandise</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </router-link>
+        </div>
+      </div>
+      <div class="col-md-3 col-10 m-2">
+        <div class="card p-4 gc text-light">
+          <router-link :to="{ name: 'Contact' }" class="btn select-borderless text-uppercase" title="Contact Us Page">
+            <h4>Catering</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </router-link>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="container-fluid">
+    <section class="row bground-img justify-content-around py-5">
+      <div class="col-10 col-md-6 d-flex align-items-center">
+        <p class="fs-5 arima"><b>Welcome to Goldy's Corner!</b> Where you'll find a leisure place to study, hang out
+          with
+          family
+          or friends,
+          have a
+          work meeting, work out of the office or enjoy the downtown scene. We have proudly serve locally owned Dawson
+          Taylor coffee since day one. We provide a variety of homemade treats, drinks, and promote
+          local artists and vendors. Local artwork is always on display.
+          Thanks for stopping by!</p>
+      </div>
+      <div class="col-10 col-md-3 second-bg text-center">
+        <p class="fw-bold lily">Coffee Shop • Café • Bakery</p>
+        <p class="fs-5"> 625 W. Main St., Boise, ID 83702 <br>
+          208.433.3934 <br>
+          Hours: Monday - Friday: 6:30am - 9:00pm Saturday: 7:00am - 9:00 Sunday: 7am - 7pm <br>
+          *Free WIFI <br>
+          *Plugins by booths and tables
+
+        </p>
+      </div>
+    </section>
+
   </div>
   <Bottom />
 </template>
 
+
+
+
+
 <script>
-import Navbar from '../components/Navbar.vue';
-
-
 export default {
   data() {
     return {
       imageIndex: 0,
       images: [
-        { src: "src/assets/img/drinks.png" },
-        { src: "src/assets/img/coffee.png" },
-        { src: "src/assets/img/food.png" }
+        { src: "src/assets/img/latte.jpg" },
+        { src: "src/assets/img/Outside.png" },
+        { src: "src/assets/img/combo.png" }
       ],
       textIndex: 0,
       texts: [
@@ -47,13 +93,9 @@ export default {
       paragraphIndex: 0,
       paragraphs: [
         "We offer one of the best happy hours in town! Come in everyday from 5:00pm - 9:00pm for half off any of our award winning mimosas and $1 off any other drink",
-        "hey hi",
-        "whats up",
+        "We take pride in our locally sourced ingredients used in all of our products.",
+        "Freshly baked goods daily made by our own in house Blaker."
       ]
-      //       HAPPY HOUR...
-
-      // We offer one of the best happy hours in town! Come in everyday from 5:00pm - 9:00pm for half off any of our award winning mimosas and $1 off any other drink
-
     };
   },
   mounted() {
@@ -75,17 +117,78 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-img {
-  height: 60dvh;
-  width: auto;
+
+<style lang="scss" scoped>
+// .image-container {
+//   height: 60vh;
+//   overflow: hidden;
+// }
+
+// .image-container img {
+//   object-fit: cover;
+//   height: 100%;
+//   width: 100%;
+// }
+
+.bground-img {
+  background-image: url(src/assets/img/WhiteParchment.png);
+  background-position: center;
+  background-size: cover;
+  box-shadow: inset 0px 10px 10px rgb(93, 91, 91);
 }
 
-.underline {
-  padding: 1rem;
-  border-width: 3px;
-  border-style: solid;
-  border-image: linear-gradient(to bottom, var(--bs-secondary),
-      #00000000) 1 100%;
+.second-bg {
+  background-color: whitesmoke;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px #3c3a3a;
+  border: solid thin black;
+}
+
+img {
+  width: 100vw;
+  height: 60vh;
+  object-fit: cover;
+  overflow: hidden;
+  border-radius: 0px 0px 25px 25px;
+  box-shadow: 0px 5px var(--bs-success);
+}
+
+.glassCard {
+  background: rgba(15, 15, 15, 0.354);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(8.1px);
+  -webkit-backdrop-filter: blur(8.1px);
+  border: 1px solid rgba(15, 15, 15, 0.26);
+  margin-right: 3rem;
+  margin-top: 5rem;
+}
+
+.gc {
+  background: rgba(15, 15, 15, 0.354);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(8.1px);
+  -webkit-backdrop-filter: blur(8.1px);
+  border: 1px solid rgba(15, 15, 15, 0.26);
+
+}
+
+@media (max-width: 768px) {
+  .slideshowContent {
+    min-height: 60vh;
+    background-position: center;
+    background-size: cover;
+    background-repeat: none;
+  }
+
+  .overlay-card {
+    width: 90%;
+  }
+}
+
+nav {
+  position: relative;
+  z-index: 1000;
 }
 </style>
---
+++ src/pages/SecondHome.vue
@@ -0,0 +1,91 @@
+<template>
+  <Navbar />
+  <div class="background-image vh-100 d-flex justify-content-center align-items-center mx-auto">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="pt-5 slideshowContent text-center slide-in underline" v-for="(text, index) in texts" :src="text"
+            :key="index" :style="{ display: textIndex === index ? 'block' : 'none' }">
+            <p class="fs-1 fw-bold arima">{{ text }} </p>
+          </div>
+          <div class="slideshowContent text-center" v-for="(paragraph, index) in paragraphs" :src="paragraph"
+            :key="index" :style="{ display: paragraphIndex === index ? 'block' : 'none' }">
+            <p class="fs-4 fw-bold">{{ paragraph }} </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="slideshowContent">
+            <img v-for="(image, index) in images" :key="index" :src="image.src"
+              :style="{ display: imageIndex === index ? 'block' : 'none' }">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Bottom />
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+
+export default {
+  data() {
+    return {
+      imageIndex: 0,
+      images: [
+        { src: "src/assets/img/drinks.png" },
+        { src: "src/assets/img/coffee.png" },
+        { src: "src/assets/img/food.png" }
+      ],
+      textIndex: 0,
+      texts: [
+        "Happy Hour",
+        "Locally Sourced Products",
+        "Fresh Baked"
+      ],
+      paragraphIndex: 0,
+      paragraphs: [
+        "We offer one of the best happy hours in town! Come in everyday from 5:00pm - 9:00pm for half off any of our award winning mimosas and $1 off any other drink",
+        "hey hi",
+        "whats up",
+      ]
+      //       HAPPY HOUR...
+
+      // We offer one of the best happy hours in town! Come in everyday from 5:00pm - 9:00pm for half off any of our award winning mimosas and $1 off any other drink
+
+    };
+  },
+  mounted() {
+    setInterval(this.nextImage, 4000);
+    setInterval(this.nextText, 4000);
+    setInterval(this.nextParagraph, 4000)
+  },
+  methods: {
+    nextImage() {
+      this.imageIndex = (this.imageIndex + 1) % this.images.length;
+    },
+    nextText() {
+      this.textIndex = (this.textIndex + 1) % this.texts.length;
+    },
+    nextParagraph() {
+      this.paragraphIndex = (this.paragraphIndex + 1) % this.paragraphs.length;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+img {
+  height: 60dvh;
+  width: auto;
+}
+
+.underline {
+  padding: 1rem;
+  border-width: 3px;
+  border-style: solid;
+  border-image: linear-gradient(to bottom, var(--bs-secondary),
+      #00000000) 1 100%;
+}
+</style>
