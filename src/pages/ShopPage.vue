18ebccbf5a69810afe20bdcce01cc2f0e51da2f3
jakew33 <jacob_winfrey@yahoo.com>
2024-07-09 21:48:53 -0600

    .

--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -2,101 +2,109 @@
   <div class="nav-container col-md-12">
     <Navbar />
   </div>
-  <main class="container-fluid background-image">
-    <div class="row p-5 justify-content-center">
+  <main class="container-fluid background-image top-page">
+    <section class="row justify-content-center">
+      <button class="filter-buttons text-uppercase" @click="changeFilter('')">All</button>
+      <button class="filter-buttons text-uppercase" @click="changeFilter(merchType)" v-for="merchType in merchTypes"
+        :key="merchType">
+        {{ merchType }}
+      </button>
+    </section>
 
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/BrownMug.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
+    <section class="row p-5 justify-content-center">
+      <div v-for="item in filteredMerch" :key="item.id" class="col-md-3 col-10 position-relative text-center my-4">
+        <div class="d-flex justify-content-center pb-5">
+          <img class="itemImage" :src="item.image" alt="">
+          <p class="description-btn text-center glassCard p-2 text-light">{{ item.description }} <br> {{ item.price }}
+          </p>
+        </div>
+        <div v-if="item.paypalButtonId">
+          <form :action="'https://www.paypal.com/cgi-bin/webscr'" method="post" target="_blank">
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" :value="item.paypalButtonId" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0"
+              name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+          </form>
+        </div>
       </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/BlueDarkblueMug.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/BlueMug.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/ClearMug.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/GreyOrangeMug.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/GroundCoffeeBeans.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/WholeCoffeeBeans.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-      <div class="col-md-3 col-10 position-relative d-flex justify-content-center my-3">
-        <img class="itemImage rounded" src="src/assets/img/ShopImages/Beanie.png" alt="">
-        <button class="btn glassCard text-light purchase-btn">Add To Cart</button>
-      </div>
-
-    </div>
+    </section>
   </main>
   <Bottom />
 </template>
 
-
 <script>
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    const merchTypes = ['mugs', 'shirts', 'cups', 'hats', 'misc'];
+    const merchItems = ref([
+      { id: 1, type: 'mugs', image: 'src/assets/img/ShopImages/BrownMug.png', description: '16 oz Brown Mug', price: '$12.95', paypalButtonId: 'NYL23AU9VCWQ2' },
+      { id: 2, type: 'mugs', image: 'src/assets/img/ShopImages/BlueDarkblueMug.png', description: '16 oz Light Blue Mug', price: '$12.95' },
+      { id: 3, type: 'mugs', image: 'src/assets/img/ShopImages/BlueMug.png', description: '16 oz Dark Blue Mug', price: '$12.95' },
+      { id: 4, type: 'mugs', image: 'src/assets/img/ShopImages/ClearMug.png', description: '12 oz Glass Mug', price: '$12.95' },
+      { id: 5, type: 'mugs', image: 'src/assets/img/ShopImages/GreyOrangeMug.png', description: '16 oz Grey/Orange Mug', price: '$12.95' },
+      { id: 6, type: 'mugs', image: 'src/assets/img/ShopImages/CornerMug.png', description: 'Goldys Corner 12 oz Mug', price: '$12.95' },
+      { id: 7, type: 'coffee', image: 'src/assets/img/ShopImages/GroundCoffeeBeans.png', description: 'Dawson Taylor Ground Coffee', price: '$15.95' },
+      { id: 8, type: 'coffee', image: 'src/assets/img/ShopImages/WholeCoffeeBeans.png', description: 'Dawson Taylor Whole Coffee Beans', price: '$15.95' },
+      { id: 9, type: 'hats', image: 'src/assets/img/ShopImages/Beanie.png', description: 'Black Beanie', price: '$18.95' },
+      { id: 10, type: 'shirts', image: 'src/assets/img/ShopImages/GrayWomansShirt.png', description: 'Gray Woman\'s Shirt', price: '$23.95' },
+      { id: 11, type: 'shirts', image: 'src/assets/img/ShopImages/PinkWomansShirt.png', description: 'Pink Woman\'s Shirt', price: '$23.95' },
+      { id: 12, type: 'shirts', image: 'src/assets/img/ShopImages/PurpleWomansShirt.png', description: 'Purple Woman\'s Shirt', price: '$23.95' },
+      { id: 13, type: 'shirts', image: 'src/assets/img/ShopImages/GrayMensShirt.png', description: 'Gray Men\'s Shirt', price: '$23.95' },
+      { id: 14, type: 'shirts', image: 'src/assets/img/ShopImages/BlueMensShirt.png', description: 'Blue Men\'s Shirt', price: '$23.95' },
+      { id: 15, type: 'misc', image: 'src/assets/img/ShopImages/Chapstick.png', description: 'Goldys Chap Stick', price: '$2.00' },
+      { id: 16, type: 'misc', image: 'src/assets/img/ShopImages/GiftCard.png', description: 'Gift Card', price: '<i>works at restaurant and corner</i>' },
+      { id: 17, type: 'misc', image: 'src/assets/img/ShopImages/ChampagneGlass.png', description: 'Add To Cart', price: '' },
+      { id: 18, type: 'misc', image: 'src/assets/img/ShopImages/WaterBottle.png', description: 'Add To Cart', price: '' },
+      { id: 19, type: 'misc', image: 'src/assets/img/ShopImages/Tumbler.png', description: 'Add To Cart', price: '' },
+      { id: 20, type: 'misc', image: 'src/assets/img/ShopImages/GoldysSpice.png', description: 'Goldys Spice Mix', price: '$12.95' },
+      { id: 21, type: 'misc', image: 'src/assets/img/ShopImages/GreenThermos.png', description: 'Add To Cart', price: '' },
+      { id: 22, type: 'misc', image: 'src/assets/img/ShopImages/OrangeThermos.png', description: 'Add To Cart', price: '' },
+      { id: 23, type: 'hats', image: 'src/assets/img/ShopImages/BlueTruckerHat1.png', description: 'Blue Trucker Hat', price: '$25.95' },
+      { id: 24, type: 'hats', image: 'src/assets/img/ShopImages/GrayTruckerHat1.png', description: 'Gray Trucker Hat', price: '$25.95' },
+      { id: 25, type: 'hats', image: 'src/assets/img/ShopImages/DadHat1.png', description: 'Yellow Dad Hat', price: '$25.95' }
+    ]);
+    const filteredMerch = ref(merchItems.value);
+
+    const changeFilter = (filter) => {
+      if (filter === '') {
+        filteredMerch.value = merchItems.value;
+      } else {
+        filteredMerch.value = merchItems.value.filter(item => item.type === filter);
+      }
+    };
+
+    return {
+      merchTypes,
+      filteredMerch,
+      changeFilter
+    };
+  }
+};
 </script>
 
-
 <style lang="scss" scoped>
-.nav-container {
-  position: relative;
-  z-index: 1000;
-  /* Increased z-index */
-}
-
-.nav {
-  z-index: 1;
-}
-
-.item-card {
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 20px;
-  margin-bottom: 20px;
-}
-
-.item-card img {
-  max-width: 300px;
-  max-height: 300px;
-  height: auto;
-}
-
-.item-title {
-  font-weight: bold;
-}
-
-.item-price {
-  color: #ffffff;
-}
-
 .itemImage {
-  height: 200px;
-  width: 200px;
-  object-fit: cover;
-  object-position: center;
+  height: 40dvh;
+  max-width: auto;
+  background-size: cover;
+  background-position: center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 20px #423826;
+  border: solid thick #00000082;
+  overflow: hidden;
+  transition: transform ease-in-out 1s;
+}
+
+.itemImage:hover {
+  box-shadow: 0px 0px 20px #2c2519;
+  transform: scale(1.05);
 }
 
 .glassCard {
-  background: rgba(15, 15, 15, 0.354);
+  background: #0f0f0f5a;
   border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(8.1px);
@@ -104,10 +112,9 @@
   border: 1px solid rgba(15, 15, 15, 0.26);
 }
 
-.purchase-btn {
+.description-btn {
   position: absolute;
-  bottom: -19px;
-  /* left: 76px; */
+  bottom: 20px;
 }
 
 .bground-img {
@@ -115,5 +122,61 @@
   background-position: center;
   background-size: cover;
   box-shadow: inset 0px 10px 10px rgb(93, 91, 91);
+}
+
+.blue-hat {
+  background-image: url(src/assets/img/ShopImages/BlueTruckerHat1.png);
+}
+
+.blue-hat:hover {
+  background-image: url(src/assets/img/ShopImages/BlueTruckerHat2.png);
+  transition: ease-in-out 1s;
+}
+
+.gray-hat {
+  background-image: url(src/assets/img/ShopImages/GrayTruckerHat1.png);
+}
+
+.gray-hat:hover {
+  background-image: url(src/assets/img/ShopImages/GrayTruckerHat2.png);
+  transition: ease-in-out 1s;
+}
+
+.dad-hat {
+  background-image: url(src/assets/img/ShopImages/DadHat1.png);
+}
+
+.dad-hat:hover {
+  background-image: url(src/assets/img/ShopImages/DadHat2.png);
+  transition: ease-in-out 1s;
+}
+
+.filter-buttons {
+  height: 6dvh;
+  width: 9dvw;
+  border-radius: 30px;
+  background-color: var(--bs-info);
+  color: var(--bs-danger);
+  font-weight: 600;
+  border: .15rem solid var(--bs-danger);
+  margin-left: .5rem;
+  margin-right: .5rem;
+}
+
+@media (max-width: 767px) {
+  .filter-buttons {
+    height: 6dvh;
+    width: 20dvw;
+    margin-top: 1rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+}
+
+.filter-buttons:hover {
+  background-color: var(--bs-danger);
+  color: var(--bs-info);
+  text-shadow: 1px 1px 2px var(--bs-info);
+  transition: ease-in-out .3s;
 }
 </style>
