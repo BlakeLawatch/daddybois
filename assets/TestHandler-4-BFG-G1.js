da593c0b543b38f2bc55665cf9bffa42d2129925
BlakeLawatch <110515565+BlakeLawatch@users.noreply.github.com>
2024-08-10 14:26:18 +0000

    Deploying to gh-pages from @ BlakeLawatch/goldys_corner@6314e9c5413d97a1592b01c0cce4d409164f057d 🚀

---
+++ assets/TestHandler-4-BFG-G1.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-Cgu6N-L8.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};
